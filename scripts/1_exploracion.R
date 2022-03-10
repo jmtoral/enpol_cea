6f56c4b317eeec86d8c64845fa4cfcb72be501d5
Manuel Toral <you@example.com>
2022-03-09 18:34:01 -0600

    Primer commit!

---
+++ scripts/1_exploracion.R
@@ -0,0 +1,10 @@
+# Análisis de la ENPOL para Proyecto LGBTI
+# Fecha de elaboración: 2022/03/09
+# Fecha de actualización: 2022/03/09
+# Autor: Manuel Toral
+
+# Bibliotecas
+
+
+
+load("data/bd_enpol_2021_RData/BD_ENPOL_2021.RData")
